bcc98c1589381ae83a6bc1177aa636d94ca7b374
FABallemand <allemand.fabien@orange.fr>
2023-05-03 19:36:16 +0200

    Astar (first part)
    Co-authored-by: PotOfJam <PotOfJam@users.noreply.github.com>

--- include/game/moveable/ghosts/path_finder.h
+++ include/game/moveable/ghosts/path_finder.h
@@ -2,14 +2,62 @@
 #define __PATH_FINDER_H__
 
 #include <array>
+#include <utility>
 #include <cmath>
+#include <queue>
+#include <vector>
+#include <functional>
 
 #include "const.h"
 #include "logger.h"
 #include "moveable.h"
 
 #define LIMIT_PATH_LENGTH 999999
+
+// Inspired by: https://stackoverflow.com/questions/16749723/how-i-can-find-value-in-priority-queue
+template <
+    class T,
+    class Container = std::vector<T>,
+    class Compare = std::less<typename Container::value_type>>
+class PriorityQueue : public std::priority_queue<T, Container, Compare>
+{
+public:
+    // typedef typename std::priority_queue<
+    //     T,
+    //     Container,
+    //     Compare>::container_type::const_iterator const_iterator;
+
+    bool find(const T &val) const
+    {
+        auto first = this->c.cbegin();
+        auto last = this->c.cend();
+        while (first != last)
+        {
+            if (*first == val)
+                return true;
+            ++first;
+        }
+        return false;
+    }
+
+    // const_iterator find(const T &val) const
+    // {
+    //     auto first = this->c.cbegin();
+    //     auto last = this->c.cend();
+    //     while (first != last)
+    //     {
+    //         if (*first == val)
+    //             return first;
+    //         ++first;
+    //     }
+    //     return last;
+    // }
+};
+
 using SimpleMaze = std::array<std::array<int, gconst::game::nb_columns>, gconst::game::nb_rows>;
+using AQueue = PriorityQueue<std::pair<int, std::pair<int, int>>, std::vector<std::pair<int, std::pair<int, int>>>, std::greater<std::pair<int, std::pair<int, int>>>>;
+using ACost = std::array<std::array<int, gconst::game::nb_columns>, gconst::game::nb_rows>;
+using Neighbors = std::array<std::pair<int, int>, 4>;
 
 /**
  * \class PathFinder
@@ -22,7 +70,7 @@ class PathFinder
 public:
     /**
      * \brief Find path to the target
-     * 
+     *
      * \param maze Representation of the maze
      * \param ghost_i Ghost row position
      * \param ghost_j Ghost column position
@@ -33,18 +81,18 @@ public:
     Direction operator()(SimpleMaze maze, int ghost_i, int ghost_j, int target_i, int target_j);
 
 private:
-    int pathFindingRec(SimpleMaze maze, int ghost_i, int ghost_j, int target_i, int target_j);
+    int pathFinderAStar(SimpleMaze maze, std::pair<int, int> ghost_position, std::pair<int, int> target_position);
+
+    int recreatePath(std::pair<int, int> current);
 
     /**
-     * \brief Compute euclidian distance between two positions on the board
+     * \brief Compute euclidian distance between two positions
      *
-     * \param i1 Row of the first point
-     * \param j1 Column of the first point
-     * \param i2 Row of the second point
-     * \param j2 Column of the second point
-     * \return int Euclidian distance bewteen the points
+     * \param position_1 First position
+     * \param position_2 Second position
+     * \return int Euclidian distance between position_1 and position_2
      */
-    int distance(int i1, int j1, int i2, int j2);
+    int distance(std::pair<int, int> position_1, std::pair<int, int> position_2);
 };
 
 #endif
--- src/game/moveable/ghosts/path_finder.cpp
+++ src/game/moveable/ghosts/path_finder.cpp
@@ -12,15 +12,15 @@ Direction PathFinder::operator()(SimpleMaze maze, int ghost_i, int ghost_j, int 
 
     // Compute the length of the shortest path to target going in th efour directions
     int path_length[4];
-    path_length[LEFT] = pathFindingRec(maze, ghost_i, ghost_j - 1, target_i, target_j);
-    path_length[RIGHT] = pathFindingRec(maze, ghost_i, ghost_j + 1, target_i, target_j);
-    path_length[UP] = pathFindingRec(maze, ghost_i - 1, ghost_j, target_i, target_j);
-    path_length[DOWN] = pathFindingRec(maze, ghost_i + 1, ghost_j, target_i, target_j);
+    // path_length[LEFT] = pathFindingRec(maze, ghost_i, ghost_j - 1, target_i, target_j);
+    // path_length[RIGHT] = pathFindingRec(maze, ghost_i, ghost_j + 1, target_i, target_j);
+    // path_length[UP] = pathFindingRec(maze, ghost_i - 1, ghost_j, target_i, target_j);
+    // path_length[DOWN] = pathFindingRec(maze, ghost_i + 1, ghost_j, target_i, target_j);
     int length = path_length[LEFT];
     Direction direction = NONE;
     for (int i = 1; i < 4; ++i)
     {
-        if(path_length[i] >= LIMIT_PATH_LENGTH)
+        if (path_length[i] >= LIMIT_PATH_LENGTH)
             continue;
         if (path_length[i] < length)
         {
@@ -32,95 +32,64 @@ Direction PathFinder::operator()(SimpleMaze maze, int ghost_i, int ghost_j, int 
     return direction;
 }
 
-int PathFinder::pathFindingRec(SimpleMaze maze, int ghost_i, int ghost_j, int target_i, int target_j)
+int PathFinder::recreatePath(std::pair<int, int> current)
 {
-    int res = LIMIT_PATH_LENGTH;
-    int tmp;
-
-    // Check if position has already been explored
-    if (maze[ghost_i][ghost_j] == 1)
-    {
-        LOG(DEBUG) << " ghost_i : " << ghost_i << " ghost_j : " << ghost_j ;
-        return res;
-    }
-    else
-    {
-        maze[ghost_i][ghost_j] = 1; // Mark the position
-    }
-
-    // Compute path length
-    if (target_i == ghost_i && target_j == ghost_j) // Already handled in wraper
-    {
-        return 0;
-    }
-    else
-    {
-        // Check if there are more than 1 possible direction
-        if (maze[ghost_i + 1][ghost_j] + maze[ghost_i][ghost_j + 1] + maze[ghost_i - 1][ghost_j] + maze[ghost_i][ghost_j - 1] < 3)
-        {
-            // Compute euclidian distance to the target from each of the neighboring positions
-            int path_length[4];
-            path_length[LEFT] = (maze[ghost_i][ghost_j - 1] == 1) ? -1 : distance(ghost_i, ghost_j - 1, target_i, target_j);
-            path_length[RIGHT] = (maze[ghost_i][ghost_j + 1] == 1) ? -1 : distance(ghost_i, ghost_j + 1, target_i, target_j);
-            path_length[UP] = (maze[ghost_i + 1][ghost_j] == 1) ? -1 : distance(ghost_i - 1, ghost_j, target_i, target_j);
-            path_length[DOWN] = (maze[ghost_i + 1][ghost_j] == 1) ? -1 : distance(ghost_i + 1, ghost_j, target_i, target_j);
-
-            int length = 99999999;
-            Direction direction = NONE;
-            for (int i = 0; i < 4; ++i)
-            {
-
-                if (path_length[i] != -1 && path_length[i] < length)
-                {
-                    direction = (Direction)i;
-                    length = path_length[i];
-                }
-            }
-            switch (direction)
-            {
-            case LEFT:
-                return pathFindingRec(maze, ghost_i, ghost_j - 1, target_i, target_j) + 1;
-                break;
-            case RIGHT:
-                return pathFindingRec(maze, ghost_i, ghost_j + 1, target_i, target_j) + 1;
-                break;
-            case UP:
-                return pathFindingRec(maze, ghost_i - 1, ghost_j, target_i, target_j) + 1;
-                break;
-            case DOWN:
-                return pathFindingRec(maze, ghost_i + 1, ghost_j, target_i, target_j) + 1;
-                break;
-            default:
-                LOG(ERROR) << "Invalid direction";
-                return -1;
-            }
-        }
-        else // Only one possible direction
-        {
-            if (maze[ghost_i - 1][ghost_j] == 0)
-            {
-                return pathFindingRec(maze, ghost_i - 1, ghost_j, target_i, target_j) + 1;
-            }
-            else if (maze[ghost_i + 1][ghost_j] == 0)
-            {
-                return pathFindingRec(maze, ghost_i + 1, ghost_j, target_i, target_j) + 1;
-            }
-            else if (maze[ghost_i][ghost_j - 1] == 0)
-            {
-                return pathFindingRec(maze, ghost_i, ghost_j - 1, target_i, target_j) + 1;
-            }
-            else if (maze[ghost_i][ghost_j + 1] == 0)
-            {
-                return pathFindingRec(maze, ghost_i, ghost_j + 1, target_i, target_j) + 1;
-            }
-        }
-    }
-    return res;
+    return 0;
 }
 
-int PathFinder::distance(int i1, int j1, int i2, int j2)
+int PathFinder::pathFinderAStar(SimpleMaze maze, std::pair<int, int> ghost_position, std::pair<int, int> target_position)
 {
-    int delta_i = (i1 - i2) * (i1 - i2);
-    int delta_j = (j1 - j2) * (j1 - j2);
+    // Initialize
+    AQueue closed_list{};
+    AQueue open_list{};
+
+    ACost cost{};
+    for (auto row : cost)
+        row.fill(LIMIT_PATH_LENGTH);
+    ACost heuristic{};
+    for (auto row : heuristic)
+        row.fill(LIMIT_PATH_LENGTH);
+
+    open_list.push({0, ghost_position});
+
+    // Main loop
+    while (!open_list.empty())
+    {
+        std::pair<int, int> current = open_list.top().second;
+        open_list.pop();
+
+        // Target reached
+        if (current == target_position)
+        {
+            return recreatePath(current); // TO DO
+        }
+
+        // Target not yet reached
+        Neighbors current_neighbors{{{ghost_position.first, ghost_position.second - 1},
+                                     {ghost_position.first, ghost_position.second + 1},
+                                     {ghost_position.first - 1, ghost_position.second},
+                                     {ghost_position.first + 1, ghost_position.second}}};
+        for (auto neighbor : current_neighbors)
+        {
+            // Neighbor is a wall
+            if (maze[neighbor.first][neighbor.second] == 1)
+                continue;
+
+            // If neighbor is not a wall
+            if (!(closed_list.find({0, neighbor}) || (open_list.find({heuristic[neighbor.first][neighbor.second], neighbor}) && cost[neighbor.first][neighbor.second] < cost[current.first][current.second])))
+            {
+                cost[neighbor.first][neighbor.second] = cost[current.first][current.second] + 1;
+                heuristic[neighbor.first][neighbor.second] = cost[current.first][current.second] + distance(neighbor, target_position);
+                open_list.push({heuristic[neighbor.first][neighbor.second], neighbor});
+            }
+        }
+        closed_list.push({0, current});
+    }
+}
+
+int PathFinder::distance(std::pair<int, int> position_1, std::pair<int, int> position_2)
+{
+    int delta_i = (position_1.first - position_2.first) * (position_1.first - position_2.first);
+    int delta_j = (position_1.second - position_2.second) * (position_1.second - position_2.second);
     return sqrt(delta_i + delta_j);
 }
