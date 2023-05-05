2c9920a89234afd234ca8785145a15a43f1e867c
slebot <samuel.lebot@etu.unistra.fr>
2023-05-05 18:01:20 +0200

    dirty fix fruit score (NEED TO BE CHANGED)

--- include/game/eatable/fruit.h
+++ include/game/eatable/fruit.h
@@ -2,6 +2,7 @@
 #define __FRUIT_H__
 
 #include "eatable.h"
+#include <array>
 
 class Game; // Forward declaration
 
@@ -21,13 +22,13 @@ class Fruit : public Eatable
 {
 public:
     Fruit(int x, int y, FruitType fruit_type) : Eatable{FRUIT, x, y, gconst::object::eatable::fruit::size, gconst::object::eatable::fruit::size, nullptr, [](int score) -> int
-                                                        { return score + gconst::object::eatable::fruit::score; }},
+                                                        { return score + gconst::object::eatable::fruit::score[0]; }},
                                                 fruit_type_{fruit_type}
     {
     }
 
 private:
-    std::array<std::array<SDL_Rect,2>8> fruit_sprite_ = {{{SDL_Rect{288,236 ,gconst::object::eatable::fruit::size_s ,gconst::object::eatable::fruit::size_s }, SDL_Rect{288,256 ,gconst::object::eatable::fruit::size_s ,gconst::object::eatable::fruit::size_s }},
+    std::array<std::array<SDL_Rect,2>,8> fruit_sprite_ = {{{SDL_Rect{288,236 ,gconst::object::eatable::fruit::size_s ,gconst::object::eatable::fruit::size_s }, SDL_Rect{288,256 ,gconst::object::eatable::fruit::size_s ,gconst::object::eatable::fruit::size_s }},
                                                           {SDL_Rect{304,236 ,gconst::object::eatable::fruit::size_s ,gconst::object::eatable::fruit::size_s }, SDL_Rect{304,256 ,gconst::object::eatable::fruit::size_s ,gconst::object::eatable::fruit::size_s }},
                                                           {SDL_Rect{320,236 ,gconst::object::eatable::fruit::size_s ,gconst::object::eatable::fruit::size_s }, SDL_Rect{320,256 ,gconst::object::eatable::fruit::size_s ,gconst::object::eatable::fruit::size_s }},
                                                           {SDL_Rect{336,236 ,gconst::object::eatable::fruit::size_s ,gconst::object::eatable::fruit::size_s }, SDL_Rect{336,256 ,gconst::object::eatable::fruit::size_s ,gconst::object::eatable::fruit::size_s }},
